ccbde489847ea110a4c5c15a3994a39e0e4e3843
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2024-06-09 19:13:52 -0300

    Extra-gas

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ menu_item.forEach((item) => {
 });
 
 function cambiarColor() {
-    const colores = ['#ADD8E6', '#FFB6C1', '#FFFFE0', '#FFF120', '#FF9003','#FFFFFF'];
+    const colores = ['#ADD8E6', '#FFB6C1', '#FFFFE0', '#FFF120', '#FF9003','#8c8c8c'];
 	const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
 	document.body.style.backgroundColor = colorAleatorio;
 }
